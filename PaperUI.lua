b7a8496c49743508eaebd51af09a7766bb8e0c00
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-11-14 11:24:08 +0200

    Custom font

--- PaperUI.lua
+++ PaperUI.lua
@@ -2,48 +2,61 @@ PaperUI = {
     Name = "PaperUI",
     Author = "Hori873",
     Version = "1.0",
-    SavedVariablesName = "PaperUI_SavedVariables"
+    SavedVariablesName = "PaperUI_SavedVariables",
+    Directories = {
+        Assets = PaperUI.Name .. "/assets",
+        Fonts = PaperUI.Directories.Assets .. "/fonts",
+        MapMarkers = PaperUI.Directories.Assets .. "/mapmarkers"
+    }
 }
 
 local textureRedirects = { }
 
 function RegisterAddonTexture(esoTexture, addonTexture)
-    textureRedirects[esoTexture] = PaperUI.Name .. "/" .. addonTexture
+    textureRedirects[esoTexture] = addonTexture
 end
 
-RegisterAddonTexture("esoui/art/icons/poi_city_complete.dds", "assets/mapmarkers/poi_city_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi_delve_complete.dds", "assets/mapmarkers/poi_delve_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi_explorable_complete.dds", "assets/mapmarkers/poi_delve_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi_keep_complete.dds", "assets/mapmarkers/poi_keep_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi_mundus_complete.dds", "assets/mapmarkers/poi_mundus_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi_tower_complete.dds", "assets/mapmarkers/poi_keep_complete.dds")
-RegisterAddonTexture("esoui/art/icons/poi/poi_wayshrine_complete.dds", "assets/mapmarkers/poi_wayshrine_complete.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_bank.dds", "assets/mapmarkers/servicepin_bank.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_caravan.dds", "assets/mapmarkers/servicepin_caravan.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_dock.dds", "assets/mapmarkers/servicepin_dock.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_fence.dds", "assets/mapmarkers/servicepin_fence.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_inn.dds", "assets/mapmarkers/servicepin_inn.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_magesguild.dds", "assets/mapmarkers/servicepin_magesguild.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_museum.dds", "assets/mapmarkers/servicepin_museum.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_outfitter.dds", "assets/mapmarkers/servicepin_outfitter.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_respecaltar.dds", "assets/mapmarkers/servicepin_respecaltar.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_stable.dds", "assets/mapmarkers/servicepin_stable.dds")
-RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_vendor.dds", "assets/mapmarkers/servicepin_vendor.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_city_complete.dds", "assets/mapmarkers/poi_city_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_delve_complete.dds", "assets/mapmarkers/poi_delve_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_explorable_complete.dds", "assets/mapmarkers/poi_delve_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_keep_complete.dds", "assets/mapmarkers/poi_keep_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_mundus_complete.dds", "assets/mapmarkers/poi_mundus_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi_tower_complete.dds", "assets/mapmarkers/poi_keep_complete.dds")
-RegisterAddonTexture("esoui/art/tutorial/poi/poi_wayshrine_complete.dds", "assets/mapmarkers/poi_wayshrine_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_city_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_city_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_delve_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_delve_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_explorable_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_delve_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_keep_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_keep_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_mundus_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_mundus_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi_tower_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_keep_complete.dds")
+RegisterAddonTexture("esoui/art/icons/poi/poi_wayshrine_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_wayshrine_complete.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_bank.dds", PaperUI.Directories.MapMarkers .. "/servicepin_bank.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_caravan.dds", PaperUI.Directories.MapMarkers .. "/servicepin_caravan.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_dock.dds", PaperUI.Directories.MapMarkers .. "/servicepin_dock.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_fence.dds", PaperUI.Directories.MapMarkers .. "/servicepin_fence.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_inn.dds", PaperUI.Directories.MapMarkers .. "/servicepin_inn.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_magesguild.dds", PaperUI.Directories.MapMarkers .. "/servicepin_magesguild.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_museum.dds", PaperUI.Directories.MapMarkers .. "/servicepin_museum.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_outfitter.dds", PaperUI.Directories.MapMarkers .. "/servicepin_outfitter.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_respecaltar.dds", PaperUI.Directories.MapMarkers .. "/servicepin_respecaltar.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_stable.dds", PaperUI.Directories.MapMarkers .. "/servicepin_stable.dds")
+RegisterAddonTexture("esoui/art/icons/servicemappins/servicepin_vendor.dds", PaperUI.Directories.MapMarkers .. "/servicepin_vendor.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_city_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_city_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_delve_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_delve_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_explorable_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_delve_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_keep_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_keep_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_mundus_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_mundus_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi_tower_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_keep_complete.dds")
+RegisterAddonTexture("esoui/art/tutorial/poi/poi_wayshrine_complete.dds", PaperUI.Directories.MapMarkers .. "/poi_wayshrine_complete.dds")
 
-function EnableTextures()
+function ChangeToPaperUI()
     for esoTexture, addonTexture in pairs(textureRedirects) do
         RedirectTexture(esoTexture, addonTexture)
     end
+
+    for key, value in zo_insecurePairs(_G) do
+        if (key):find("^Zo") and type(value) == "userdata" and value.SetFont then
+           local font = {value:GetFontInfo()}
+           font[1] = PaperUI.Directories.Fonts .. "/Kingthings_Exeter.ttf"
+           value:SetFont(table.concat(font, "|"))
+        end
+     end
 end
 
-function DisableTextures()
+function ChangeToVanillaUI()
     for esoTexture, addonTexture in pairs(textureRedirects) do
         RedirectTexture(esoTexture, esoTexture)
     end
@@ -61,9 +74,9 @@ function OnAddOnLoaded(event, addonName)
     PaperUI.SavedVariables = ZO_SavedVars:NewAccountWide(PaperUI.SavedVariablesName, ADDON_VERSION, defaults, nil)
 
     if PaperUI.SavedVariables.ActiveUI == "Vanilla" then
-        DisableTextures()
+        ChangeToVanillaUI()
     elseif PaperUI.SavedVariables.ActiveUI == PaperUI.Name then
-        EnableTextures()
+        ChangeToPaperUI()
     end
 
     PaperUI:InitialiseAddonMenu()
